5802e2a251496c3d221158a92bddb05da1b32119
Maya Santiago <69778090+msatori@users.noreply.github.com>
2020-12-13 23:21:40 -0600

    created delete and update routes for tags and categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -49,19 +49,54 @@ router.post('/', (req, res) => {
   Category.create({
     category_name: req.body.category_name,
   })
-  .then(dbCategoryData => res.json(dbCategoryData))
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-});
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  ).then(dbCategoryData => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: 'No category found  with this id' });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(dbCategoryData => {
+    if(!dbCategoryData) {
+      res.status(404).json({ message: 'No category found with this id' });
+      return
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  })
 });
 
 module.exports = router;
